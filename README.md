2d5308b919c07e9591d2892bea670026f43a92b1
seregasohigh <ser191298@gmail.com>
2023-05-17 21:57:14 +0300

    написал игру-калькулятор brain-calc

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/seregasohigh/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/seregasohigh/python-project-49/actions)
 <a href="https://codeclimate.com/github/seregasohigh/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/b067ed48db53282d0b36/maintainability" /></a>
 https://asciinema.org/a/xLSBixPhgkSa2or3ZSTSvGlxs
+https://asciinema.org/a/zjyMVy2kOVVK4FLs69WWijIxr
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import random
+import prompt
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May i have your name? ')
+    print(f"Hello, {name}!")
+    return name
+
+
+def generate_question():
+    a = random.randint(0, 100)
+    b = random.randint(0, 100)
+    operator = random.choice(['+', '-', '*'])
+    if operator == '+':
+        result = a + b
+    elif operator == '-':
+        result = a - b
+    else:
+        result = a * b
+    return (f'Question: {a} {operator} {b}', result)
+
+
+def get_user_answer():
+    while True:
+        answer = input('Your answer: ')
+        try:
+            answer = int(answer)
+            return answer
+        except ValueError:
+            print('Please enter a number')
+
+
+def main():
+    name = welcome_user()
+    print('What is the result of the expression?')
+    for _ in range(3):
+        question, result = generate_question()
+        print(question)
+        answer = get_user_answer()
+        if answer == result:
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(.\
+             Correct answer was '{result}'. Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == '__main__':
+    main()
