1fbdbe2281f5fcab90177924da3b3f3384b9bcae
seregasohigh <ser191298@gmail.com>
2023-05-18 22:28:07 +0300

    создал игру greatest common divisor

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 <a href="https://codeclimate.com/github/seregasohigh/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/b067ed48db53282d0b36/maintainability" /></a>
 https://asciinema.org/a/xLSBixPhgkSa2or3ZSTSvGlxs
 https://asciinema.org/a/zjyMVy2kOVVK4FLs69WWijIxr
+https://asciinema.org/a/oiyj45cKgB3RdiJtsZGmpvivG
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import random
+import prompt
+
+
+def welcome_user():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May i have your name? ')
+    print(f"Hello, {name}!")
+    return name
+
+
+def generate_question():
+    a = random.randint(1, 100)
+    b = random.randint(1, 100)
+    return a, b
+
+
+def result(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def main():
+    name = welcome_user()
+    print('Find the greatest common divisor of given numbers.')
+    for _ in range(3):
+        a, b = generate_question()
+        print(f'Question: {a} {b}')
+        answer = input('Your answer: ')
+        correct_answer = str(result(a, b))
+        if answer == correct_answer:
+            print('Correct!')
+        else:
+            print(f"'{answer}' is wrong answer ;(.\
+             Correct answer was '{correct_answer}'. Let's try again, {name}!")
+            return
+    print(f"Congratulations, {name}!")
+
+
+if __name__ == '__main__':
+    main()
