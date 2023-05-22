66c0644fe7473803b29a3806db716966efe4fd97
seregasohigh <ser191298@gmail.com>
2023-05-22 21:25:13 +0300

    поправил ошибки

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-import prompt
 import random
+import prompt
 
 
 def welcome_user():
     print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
+    name = prompt.string('May i have your name? ')
     print(f"Hello, {name}!")
     return name
 
